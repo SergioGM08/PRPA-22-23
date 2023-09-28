6f55310293a42842404098b53238566e14431ea6
Sergio González Montero <123453130+SergioGM08@users.noreply.github.com>
2023-09-28 17:51:56 +0200

    Add files via upload

---
+++ Practica_4_Bicimad_analysis/descargaTodos.bat
@@ -0,0 +1,5 @@
+:start
+ for /l %%G in (2017,1,2023) DO py .\descargarDatosYear.py %%G
+
+:end
+exit
---
+++ Practica_4_Bicimad_analysis/descargaTodos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in {2017, 2023}
+do
+   python3 .\descargarDatosYear.py $i
+done
